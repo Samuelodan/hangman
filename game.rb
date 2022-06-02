45763267829bf78a644f55c5921aff229c54ebcc
Samuel ODaniels <samodan195@gmail.com>
2022-06-02 15:42:34 +0100

    Write method to set chances according to chosen_word length

--- game.rb
+++ game.rb
@@ -2,7 +2,14 @@ require './board'
 
 # handles the flow of the game
 class Game
+  attr_reader :board, :chances
+
   def initialize
     @board = Board.new
+    @chances = nil
+  end
+
+  def set_chances
+    self.chances = board.chosen_word.length * 1.5
   end
 end
